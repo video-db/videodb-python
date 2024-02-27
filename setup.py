e78054cda8c7feb28c7cd5f11ff30aa9258c2fe3
Ankit raj <113342181+ankit-v2-3@users.noreply.github.com>
2024-02-27 16:08:45 +0530

    feat: add pypi classifiers

--- setup.py
+++ setup.py
@@ -37,6 +37,11 @@ setup(
     classifiers=[
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8.1",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "License :: OSI Approved :: Apache Software License",
     ],
 )
