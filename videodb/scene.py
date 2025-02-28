f11185f2d57d81aa566540b2478be24fb30c563e
Ankit raj <113342181+ankit-v2-3@users.noreply.github.com>
2025-02-28 15:33:14 +0530

    feat: add scene metadata

--- videodb/scene.py
+++ videodb/scene.py
@@ -14,6 +14,7 @@ class Scene:
         description: str,
         id: str = None,
         frames: List[Frame] = [],
+        metadata: dict = {},
         connection=None,
     ):
         self.id = id
@@ -22,6 +23,7 @@ class Scene:
         self.end = end
         self.frames: List[Frame] = frames
         self.description = description
+        self.metadata = metadata
         self._connection = connection
 
     def __repr__(self) -> str:
@@ -32,7 +34,8 @@ class Scene:
             f"start={self.start}, "
             f"end={self.end}, "
             f"frames={self.frames}, "
-            f"description={self.description})"
+            f"description={self.description}), "
+            f"metadata={self.metadata})"
         )
 
     def to_json(self):
@@ -43,6 +46,7 @@ class Scene:
             "end": self.end,
             "frames": [frame.to_json() for frame in self.frames],
             "description": self.description,
+            "metadata": self.metadata,
         }
 
     def describe(self, prompt: str = None, model_name=None) -> None:
